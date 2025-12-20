eb0896988a724375056e40f69f16f47b0f31c59c
ahmedabirdev <ahmedabir2852@gmail.com>
2025-12-20 13:59:30 +0530

    backedn url fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:7070/",
-  baseURL: "https://internshala-backend-ucyy.onrender.com",
+  baseURL: "https://internshala-backend-ef6q.onrender.com",
   withCredentials: true,
 });
 
